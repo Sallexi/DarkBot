b739450754dc7e14eef0e73161e97f5eb4f42a01
Xeronel <ripster1121@gmail.com>
2016-10-23 23:34:33 -0500

    Add user tracker

--- twitch.py
+++ twitch.py
@@ -1,15 +1,17 @@
 import socket
 import tornado
 import tornado.iostream
+import tornado.gen
 from tornado.ioloop import IOLoop
 from tornado import httpclient
+from datetime import datetime
 import dateutil.parser
 import inspect
 import json
 
 
 class Twitch(object):
-    def __init__(self, username, password, client_id):
+    def __init__(self, username, password, client_id, db):
         self.server_address = 'irc.twitch.tv'
         self.port = 6667
         self.username = username
@@ -18,17 +20,23 @@ class Twitch(object):
         self.encoding = 'utf-8'
         self.ioloop = IOLoop.current()
         self.followers = {}
-        httpclient.AsyncHTTPClient.configure('tornado.curl_httpclient.CurlAsyncHTTPClient')
-        self.web_client = httpclient.AsyncHTTPClient()
+        self.chatters_last_update = None
+        self.db = db
         self.__handlers = {b'PING': self.ping,
                            b'376': self.motd_end,
                            b'PRIVMSG': self.message}
 
-        # Initially load followers
-        self.load_followers(say_new=False)
+        # Create a web client
+        httpclient.AsyncHTTPClient.configure('tornado.curl_httpclient.CurlAsyncHTTPClient')
+        self.web_client = httpclient.AsyncHTTPClient()
 
-        # Check followers every 1 minute
+        # Initially load data
+        self.load_followers(say_new=False)
+        self.load_chatters()
+
+        # Check every 1 minute
         tornado.ioloop.PeriodicCallback(self.load_followers, 60000).start()
+        tornado.ioloop.PeriodicCallback(self.load_chatters, 60000).start()
 
         # Create a new TCP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -87,12 +95,12 @@ class Twitch(object):
 
     def ping(self, message):
         response = 'PONG %s' % message.decode(self.encoding)
-        print(response)
         self.send(response)
 
     def motd_end(self, message):
         self.send('JOIN #darkvalkyrieprincess')
 
+    @tornado.gen.coroutine
     def message(self, message, params, prefix):
         # Convert to strings
         user = prefix.decode(self.encoding)
@@ -104,13 +112,23 @@ class Twitch(object):
         user = user[:user.find('!')]
 
         if message == '!followers':
-            self.fetch('/channels/darkvalkyrieprincess/follows', self.say_followers)
-        if message == '!top5':
+            self.fetch_api('/channels/darkvalkyrieprincess/follows', self.say_followers)
+        elif message == '!top5':
             top_users = sorted(self.followers, key=(lambda user: self.followers[user]['follow_date']))[:5]
             top_users = [(rank + 1, self.get_name(uid)) for rank, uid in enumerate(top_users)]
             top_users = ' '.join('%s. %s' % user for user in top_users)
             self.say(top_users, channel)
-
+        elif message == '!topviewer':
+            result = yield self.db.execute(
+                """
+                SELECT username, watch_time
+                FROM darkbot.users
+                WHERE username <> 'darkvalkyrieprincess'
+                ORDER BY watch_time DESC
+                LIMIT 1
+                """)
+            result = result.fetchone()
+            self.say('%s has watched for a total of %s hours!!' % (result[0], round(result[1]/3600, 2)), channel)
         print('%s : %s : %s' % (channel, user, message))
 
     def get_name(self, user_id):
@@ -124,8 +142,47 @@ class Twitch(object):
     def say(self, message: str, target: str):
         self.send('PRIVMSG %s :%s' % (target, message))
 
+    @tornado.gen.coroutine
+    def load_chatters(self, response=None):
+        if response and not response.error:
+            chatters = json.loads(response.body.decode(self.encoding))
+            users = []
+
+            if self.chatters_last_update is not None:
+                time_delta = datetime.now() - self.chatters_last_update
+            else:
+                time_delta = datetime.now() - datetime.now()
+
+            for group in chatters['chatters']:
+                for user in chatters['chatters'][group]:
+                    users.append(user)
+                    users.append(group)
+                    users.append(time_delta.seconds)
+                    users.append(datetime.utcnow())
+                    users.append(datetime.utcnow())
+
+            if len(users) > 1:
+                values = ' (%s, %s, %s, %s, %s),' * int(len(users) / 5)
+                values = values[:-1]
+            else:
+                values = ' (%s %s %s %s %s)'
+
+            self.chatters_last_update = datetime.now()
+            yield self.db.execute(
+                "INSERT INTO darkbot.users (username, chat_group, watch_time, first_noticed, last_noticed) "
+                "VALUES" + values +
+                " ON CONFLICT (username) "
+                "DO UPDATE SET "
+                "chat_group = EXCLUDED.chat_group, "
+                "watch_time = EXCLUDED.watch_time + users.watch_time, "
+                "last_noticed = EXCLUDED.last_noticed",
+                users)
+            print(datetime.now().strftime('%I:%M') + ' Updated chatters')
+        else:
+            self.fetch_tmi('/group/user/darkvalkyrieprincess/chatters', self.load_chatters)
+
     def load_followers(self, say_new=True):
-        self.fetch('/channels/darkvalkyrieprincess/follows', lambda x: self.check_followers(x, say_new=say_new))
+        self.fetch_api('/channels/darkvalkyrieprincess/follows', lambda x: self.check_followers(x, say_new=say_new))
 
     def check_followers(self, response=None, say_new=True):
         if response and not response.error:
@@ -142,7 +199,17 @@ class Twitch(object):
                     self.say('%s is now following!' % users[user_id]['name'], '#darkvalkyrieprincess')
             self.followers = users
 
-    def fetch(self, url, handler):
+    def fetch_tmi(self, url, handler):
+        if not url.startswith('/'):
+            url = '/' + url
+
+        self.web_client.fetch('https://tmi.twitch.tv' + url,
+                              handler,
+                              validate_cert=False,
+                              headers={'Accept': 'application/vnd.twitchtv.v3+json',
+                                       'Client-ID': self.client_id})
+
+    def fetch_api(self, url, handler):
         if not url.startswith('/'):
             url = '/' + url
 
