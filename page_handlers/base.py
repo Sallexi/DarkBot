411a0a2c0ac2cde3926016626aebf63f6a1180e9
Xeronel <ripster1121@gmail.com>
2016-10-30 13:43:05 -0400

    Keep nav state on page load

--- page_handlers/base.py
+++ page_handlers/base.py
@@ -23,3 +23,13 @@ class BaseHandler(tornado.web.RequestHandler):
         else:
             result = {}
         return result
+
+    @property
+    def nav_minimized(self):
+        nav_minimized = str(self.get_cookie('nav_minimized', 'false')).lower()
+        return True if nav_minimized == 'true' else False
+
+    def render(self, template_name, **kwargs):
+        if 'nav_minimized' not in kwargs:
+            kwargs['nav_minimized'] = self.nav_minimized
+        super(BaseHandler, self).render(template_name, **kwargs)
--- web/static/js/darkbot.js
+++ web/static/js/darkbot.js
@@ -1,12 +1,29 @@
 var darkbot = {};
-
 darkbot.navSelector = function (id) {
     $("nav ul li").removeClass("active");
     $('#' + id).addClass('active');
 };
 
-darkbot.toggleSidebar = function () {
-    $('#left-panel').toggleClass('minimized');
-    $('#center-panel').toggleClass('expanded');
-    $('#arrow').toggleClass('arrow-right');
+darkbot.toggleSidebar = function (update_cookie) {
+    var leftPanel = $('#left-panel');
+    var centerPanel = $('#center-panel');
+    var arrow = $('#arrow');
+
+    leftPanel.toggleClass('minimized');
+    centerPanel.toggleClass('expanded');
+    arrow.toggleClass('arrow-right');
+
+    if (leftPanel.hasClass('minimized')) {
+        Cookies.set('nav_minimized', true);
+    } else {
+        Cookies.set('nav_minimized', false);
+    }
+};
+
+darkbot.toggleCookie = function (cookie) {
+    if (Cookies.get(cookie) === "true") {
+        Cookies.set(cookie, false);
+    } else {
+        Cookies.set(cookie, true);
+    }
 };
